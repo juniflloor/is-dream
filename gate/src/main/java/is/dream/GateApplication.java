958753200ef96a06beab6db125371936198c7703
chendongzhao <2457364447@qq.com>
2020-06-08 21:45:01 +0800

    registation eureka gate service

--- gate/src/main/java/is/dream/GateApplication.java
+++ gate/src/main/java/is/dream/GateApplication.java
@@ -2,14 +2,14 @@ package is.dream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author chendongzhao
  * @version 1.0
  * @date 2020/6/8 10:23
  */
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GateApplication {
       public static void main(String [] args) { SpringApplication.run(GateApplication.class,args);}
